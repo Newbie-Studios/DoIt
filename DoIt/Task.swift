38fb44ffc1319f3685df0f9d37bd8a70d529d238
Jamie Roberts <jamieroberts0890@gmail.com>
2017-09-23 14:50:46 +0100

    Create first view controller for table view

---
+++ DoIt/Task.swift
@@ -0,0 +1,17 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Jamie Roberts on 23/09/2017.
+//  Copyright © 2017 Newbie-Studios. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+    
+}
+
+
--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -8,18 +8,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+   
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tasks : [Task] = [] //create a variable called tasks which is of the Task type equal to an empty array
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib
+        
+        
+        tasks = makeTasks() //reassign our tasks variable equal to our function makeTasks (this needs to be before datasource and delegate)
+        
+        tableView.dataSource = self //tableview requires a datasource = self
+        tableView.delegate = self //tableview requires a delegate = self
+        
+    }
+ 
+    
+    //TABLEVIEW ALSO REQUIRES TWO FUNCTION - NUMBER OF ROWS IN SECTION AND CELL FOR ROW AT
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count //now returning count of tasks variable
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell() //cellForRowAt requires a UITableViewCell to be returned so we create constant 'cell'
+        let task = tasks[indexPath.row]//create constant task which is equal to indexpath.row of tasks variable
+        if task.important {
+                  cell.textLabel?.text = "‼‼ \(task.name) ‼‼"
+        } else {
+                 cell.textLabel?.text = task.name
+        } //added an if statement to say if Important flag is true, add some !!, if not don't do that
+        return cell //returning that value
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
+    
+    
+    func makeTasks() -> [Task] {
+        let task1 = Task()
+        task1.name = "Walk the Cat"
+        task1.important = false
+        
+        let task2 = Task()
+        task2.name = "Wash the dishes"
+        task2.important = true
+        
+        let task3 = Task()
+        task3.name = "Hoover"
+        task3.important = false
+        
+        return [task1,task2,task3]
+        
+    } //create function that includes all our tasks we're making
 
 }
 
